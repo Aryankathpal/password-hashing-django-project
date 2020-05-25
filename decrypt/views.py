b23f672eccd20d4fd1931da83fa99e5adadf00e2
Aryankathpal <aryankathpal.cse19@chitkarauniversity.edu.in>
2020-05-25 13:01:14 +0530

    almost

--- decrypt/views.py
+++ decrypt/views.py
@@ -12,14 +12,16 @@ from encrypt.models import encoded
 def decoded(request):
     decrypt_form=decrypt(request.POST)
     hash=''
+    hash1=''
     if request.method=='POST':
         b=keyid.objects.get(user_id=request.user)
         key=b.keys
         if decrypt_form.is_valid():
             pas = hashcode(id=key,password=request.POST['encrypted_password'])
             hash=pas
-
-    return render(request, 'decrypt/decrypt.html',{'decrypt_form':decrypt_form,'hash':hash})
+        else:
+            hash1='invalid hash'
+    return render(request, 'decrypt/decrypt.html',{'decrypt_form':decrypt_form,'hash':hash,'hash1':hash1})
 
 @login_required
 def endecrypt(request, hash_id):
@@ -27,5 +29,5 @@ def endecrypt(request, hash_id):
      b=keyid.objects.get(user_id=request.user)
      key=b.keys
      pas = hashcode(id=key,password=hash.enc)
-     hash=pas
-     return render(request,'decrypt/endecrypt.html',{'hash':hash})
+     hash1=pas
+     return render(request,'decrypt/endecrypt.html',{'hash1':hash1,'hash':hash})
